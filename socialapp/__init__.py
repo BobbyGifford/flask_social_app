b6bb0d6f071359e1c793680920312207d68de412
Bobby <bobby077joe@gmail.com>
2018-09-19 19:48:39 -0700

    Adding user views.

--- socialapp/__init__.py
+++ socialapp/__init__.py
@@ -20,7 +20,9 @@ db = SQLAlchemy(app)
 Migrate(app, db)
 
 from socialapp.core.views import core
+from socialapp.users.views import users
 from socialapp.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
